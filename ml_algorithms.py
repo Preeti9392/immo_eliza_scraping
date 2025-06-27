78b924571b6d488739a13c51af1cc1a89e66ea2e
Preeti <preetiduhan9392@gmail.com>
2025-06-27 11:14:43 +0200

    rearrangment

---
+++ ml_algorithms.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from  sklearn.preprocessing import LabelEncoder 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import accuracy_score, mean_absolute_percentage_error, mean_squared_error, r2_score
+#from  typing import
